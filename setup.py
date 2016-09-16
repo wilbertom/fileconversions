1cad4b7569a0c9a9ed0540323c2c0b3755082426
Wilberto Morales <wilbertomorales777@gmail.com>
2016-09-15 23:38:38 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fileconversions',
+    version='0.1.0.0',
+    packages=['fileconversions', 'fileconversions.conversions'],
+    url='',
+    license='MIT',
+    author='Wilberto Morales',
+    author_email='wilbertomorales777@gmail.com',
+    description='Various file format conversions.'
+)
